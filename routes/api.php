47a83dadcf240a8223110a686a82ace7e4d79209
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-08-07 23:37:06 +0800

    refactor: changed html title and added versioning in routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TouristController;
 use App\Models\Admin;
 use App\Models\Establishment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\Comparator\Factory;
 use App\Models\User;
@@ -21,9 +22,11 @@ use App\Models\Tourist;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('/login', 'login');
@@ -38,17 +41,17 @@ Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Don't take it seriously. 
 */
 
-Route::get('/users', function () {
+Route::get('/v1/users', function () {
     $users = User::all();
     return $users;
-});
+})->middleware('auth:sanctum');
 
-Route::get('/tourists', function () {
+Route::get('/v1/tourists', function () {
     $tourists = Tourist::all();
     return view('tourists.index', ['tourists' => $tourists]);
 });
 
-Route::get('/tourist/{id}', function ($id) {
+Route::get('/v1/tourist/{id}', function ($id) {
     $tourist = Tourist::find($id);
     if ($tourist) {
         return view('tourists.tourist', ['tourist' => $tourist]);
@@ -56,28 +59,28 @@ Route::get('/tourist/{id}', function ($id) {
     abort(404);
 });
 
-Route::get('/establishment/{id}', function ($id) {
+Route::get('/v1/establishment/{id}', function ($id) {
     $est = Establishment::find($id);
     return view('establishment.index', ['est' => $est]);
 });
 
-Route::get('/admin', function () {
+Route::get('/v1/admin', function () {
     $admins = Admin::all();
     return view('admin.index', ['admins' => $admins]);
 });
 
-Route::get('/admin/{id}', function ($id) {
+Route::get('/v1/admin/{id}', function ($id) {
     $admin = Admin::find($id);
     return view('admin.admin', ['admin' => $admin]);
 });
 
 // generate fake users
-Route::get('/generateUsers/{count}', function ($count) {
+Route::get('/v1/generateUsers/{count}', function ($count) {
     User::factory()->count($count)->create();
 });
 
 // generate fake tourists
-Route::get('/generateTourists/{count}', function ($count) {
+Route::get('/v1/generateTourists/{count}', function ($count) {
     Tourist::factory()->count($count)->create();
 });
 
