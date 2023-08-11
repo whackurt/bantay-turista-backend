6769d7430b5d5afab7dcabc70573e35060da905a
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-08-11 10:49:36 +0800

    refactor: modified Logs blade

--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
     <div>
-        <p>{{$log}}</p>
-        <p>{{$log[0]->tourist->first_name}} {{$log[0]->tourist->last_name}}</p>
-        <p>{{$log[0]->establishment->name}}</p>
-        <p>{{$log[0]->establishment->owner_name}}</p>
+        @foreach ($logs as $log)
+            <strong style="color: brown">{{$log}}</strong>
+            <h2 style="color: brown">{{$log->tourist->last_name}}, {{$log->tourist->first_name}} </h2>
+            <h3>{{$log->tourist->qr_code}}</h3>
+            <p>Establishment: {{$log->establishment->name}}</p>
+            <p>Establishment Owner: {{$log->establishment->owner_name}}</p>
+            <hr>
+        @endforeach
     </div>
 @endsection
